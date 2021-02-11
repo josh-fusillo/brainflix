21774480655c6aeed324b7e4e195b41a3d00c451
Josh Fusillo <josh.fusillo@gmail.com>
2021-02-10 19:33:49 -0500

    Updates

---
+++ precache-manifest.02d367bef6bee8cd8b0b996a3a65cdbb.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f191363cc91bffc923f06d1cffbcfce6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c37870eacc1ed9050cb",
+    "url": "/static/css/main.e9ab0a88.chunk.css"
+  },
+  {
+    "revision": "9ed3d58db32fdae0bf93",
+    "url": "/static/js/2.2fe12dee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.2fe12dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c37870eacc1ed9050cb",
+    "url": "/static/js/main.83610466.chunk.js"
+  },
+  {
+    "revision": "520c89ab5f69f088f843",
+    "url": "/static/js/runtime-main.36c1f420.js"
+  },
+  {
+    "revision": "482a9149a783989a39a397e2ebf5373f",
+    "url": "/static/media/Icon-fullscreen.482a9149.svg"
+  },
+  {
+    "revision": "4895cd47751aee213c4343fbf9bae83e",
+    "url": "/static/media/Icon-likes.4895cd47.svg"
+  },
+  {
+    "revision": "edecac9e1d5e5e5d29acd5ad2481fb3f",
+    "url": "/static/media/Icon-play.edecac9e.svg"
+  },
+  {
+    "revision": "998eed46b18a86cebb99b01236a53b80",
+    "url": "/static/media/Icon-search.998eed46.svg"
+  },
+  {
+    "revision": "1d3309e008b2977368ae8180cdd2bcd5",
+    "url": "/static/media/Icon-upload.1d3309e0.svg"
+  },
+  {
+    "revision": "2ceaa231afc40cc73831bec20484968f",
+    "url": "/static/media/Icon-views.2ceaa231.svg"
+  },
+  {
+    "revision": "a1be2da0309b517c103ab1a65854d0ac",
+    "url": "/static/media/Icon-volume.a1be2da0.svg"
+  },
+  {
+    "revision": "222586f42fa280e476462914cdf70dec",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Bold.222586f4.otf"
+  },
+  {
+    "revision": "1a1c2c769bb1a94529f507e96d6985ac",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Demi.1a1c2c76.otf"
+  },
+  {
+    "revision": "67016125d8abf229098a0195c7f9ed64",
+    "url": "/static/media/Linotype-AvenirNextLTPro-Regular.67016125.otf"
+  },
+  {
+    "revision": "9049851c0871c1ffa20f8b402e6531b8",
+    "url": "/static/media/Logo-brainflix.9049851c.svg"
+  },
+  {
+    "revision": "ba2f48f9885e020bc9bf2c8c442af56a",
+    "url": "/static/media/Mohan-muruge.ba2f48f9.jpg"
+  },
+  {
+    "revision": "c814c81c34ab0a6574dffc33fcec7fa1",
+    "url": "/static/media/Upload-video-preview.c814c81c.jpg"
+  }
+]);
