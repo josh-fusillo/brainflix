83b85a7670bcecd54819167d34ba6ad52651718f
Josh Fusillo <josh.fusillo@gmail.com>
2021-02-10 19:34:31 -0500

    Updates

---
+++ precache-manifest.6944d92d96b84a35fe92c26c5ec01dbf.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5c46a52a918c012795f223b45fc63c6",
+    "url": "/brainflixx/index.html"
+  },
+  {
+    "revision": "9342633e7475d2ff0834",
+    "url": "/brainflixx/static/css/main.56bc0b11.chunk.css"
+  },
+  {
+    "revision": "9ed3d58db32fdae0bf93",
+    "url": "/brainflixx/static/js/2.2fe12dee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brainflixx/static/js/2.2fe12dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9342633e7475d2ff0834",
+    "url": "/brainflixx/static/js/main.f22982e1.chunk.js"
+  },
+  {
+    "revision": "3525163963fc9e21386f",
+    "url": "/brainflixx/static/js/runtime-main.13102127.js"
+  },
+  {
+    "revision": "482a9149a783989a39a397e2ebf5373f",
+    "url": "/brainflixx/static/media/Icon-fullscreen.482a9149.svg"
+  },
+  {
+    "revision": "4895cd47751aee213c4343fbf9bae83e",
+    "url": "/brainflixx/static/media/Icon-likes.4895cd47.svg"
+  },
+  {
+    "revision": "edecac9e1d5e5e5d29acd5ad2481fb3f",
+    "url": "/brainflixx/static/media/Icon-play.edecac9e.svg"
+  },
+  {
+    "revision": "998eed46b18a86cebb99b01236a53b80",
+    "url": "/brainflixx/static/media/Icon-search.998eed46.svg"
+  },
+  {
+    "revision": "1d3309e008b2977368ae8180cdd2bcd5",
+    "url": "/brainflixx/static/media/Icon-upload.1d3309e0.svg"
+  },
+  {
+    "revision": "2ceaa231afc40cc73831bec20484968f",
+    "url": "/brainflixx/static/media/Icon-views.2ceaa231.svg"
+  },
+  {
+    "revision": "a1be2da0309b517c103ab1a65854d0ac",
+    "url": "/brainflixx/static/media/Icon-volume.a1be2da0.svg"
+  },
+  {
+    "revision": "222586f42fa280e476462914cdf70dec",
+    "url": "/brainflixx/static/media/Linotype-AvenirNextLTPro-Bold.222586f4.otf"
+  },
+  {
+    "revision": "1a1c2c769bb1a94529f507e96d6985ac",
+    "url": "/brainflixx/static/media/Linotype-AvenirNextLTPro-Demi.1a1c2c76.otf"
+  },
+  {
+    "revision": "67016125d8abf229098a0195c7f9ed64",
+    "url": "/brainflixx/static/media/Linotype-AvenirNextLTPro-Regular.67016125.otf"
+  },
+  {
+    "revision": "9049851c0871c1ffa20f8b402e6531b8",
+    "url": "/brainflixx/static/media/Logo-brainflix.9049851c.svg"
+  },
+  {
+    "revision": "ba2f48f9885e020bc9bf2c8c442af56a",
+    "url": "/brainflixx/static/media/Mohan-muruge.ba2f48f9.jpg"
+  },
+  {
+    "revision": "c814c81c34ab0a6574dffc33fcec7fa1",
+    "url": "/brainflixx/static/media/Upload-video-preview.c814c81c.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.02d367bef6bee8cd8b0b996a3a65cdbb.js"
+  "/brainflixx/precache-manifest.6944d92d96b84a35fe92c26c5ec01dbf.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/brainflixx/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
