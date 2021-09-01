be0d1d6de361f0a5125756a513a9e117b112aa19
shadow104 <josh.fusillo@gmail.com>
2021-08-31 19:01:03 -0700

    fixed some broken styles on the scrubber area

---
+++ build/precache-manifest.6e0e135397fecc43979e73dc5a2a11d0.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "482486a854c05c1783f74493df8d1338",
+    "url": "/brainflix/index.html"
+  },
+  {
+    "revision": "e1c017042a0e3a71ce35",
+    "url": "/brainflix/static/css/main.5cc1208c.chunk.css"
+  },
+  {
+    "revision": "9ed3d58db32fdae0bf93",
+    "url": "/brainflix/static/js/2.2fe12dee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/brainflix/static/js/2.2fe12dee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1c017042a0e3a71ce35",
+    "url": "/brainflix/static/js/main.7d359b8f.chunk.js"
+  },
+  {
+    "revision": "ed959d860794dc47be3d",
+    "url": "/brainflix/static/js/runtime-main.13ef5770.js"
+  },
+  {
+    "revision": "03b74b4c4dc7cd48eee599a4517f1441",
+    "url": "/brainflix/static/media/Icon-fullscreen.03b74b4c.svg"
+  },
+  {
+    "revision": "23c3f8579420d52ffd5c13ad742baa6a",
+    "url": "/brainflix/static/media/Icon-likes.23c3f857.svg"
+  },
+  {
+    "revision": "a3c51b76d2b01afe928cef3c6b522f21",
+    "url": "/brainflix/static/media/Icon-play.a3c51b76.svg"
+  },
+  {
+    "revision": "286069001111ea3ed233483011ce0c09",
+    "url": "/brainflix/static/media/Icon-search.28606900.svg"
+  },
+  {
+    "revision": "909b8976a003b06080b276c94fcfe418",
+    "url": "/brainflix/static/media/Icon-upload.909b8976.svg"
+  },
+  {
+    "revision": "a488fc5b5ba8a1b910c6b8278131ffac",
+    "url": "/brainflix/static/media/Icon-views.a488fc5b.svg"
+  },
+  {
+    "revision": "d17c2fe7a1d599b46d1ce25f73786da2",
+    "url": "/brainflix/static/media/Icon-volume.d17c2fe7.svg"
+  },
+  {
+    "revision": "222586f42fa280e476462914cdf70dec",
+    "url": "/brainflix/static/media/Linotype-AvenirNextLTPro-Bold.222586f4.otf"
+  },
+  {
+    "revision": "1a1c2c769bb1a94529f507e96d6985ac",
+    "url": "/brainflix/static/media/Linotype-AvenirNextLTPro-Demi.1a1c2c76.otf"
+  },
+  {
+    "revision": "67016125d8abf229098a0195c7f9ed64",
+    "url": "/brainflix/static/media/Linotype-AvenirNextLTPro-Regular.67016125.otf"
+  },
+  {
+    "revision": "1e121e6bb74d3a6c39ed045da009ca40",
+    "url": "/brainflix/static/media/Logo-brainflix.1e121e6b.svg"
+  },
+  {
+    "revision": "ba2f48f9885e020bc9bf2c8c442af56a",
+    "url": "/brainflix/static/media/Mohan-muruge.ba2f48f9.jpg"
+  },
+  {
+    "revision": "c814c81c34ab0a6574dffc33fcec7fa1",
+    "url": "/brainflix/static/media/Upload-video-preview.c814c81c.jpg"
+  }
+]);
